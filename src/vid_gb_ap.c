0d475d60def684e82e23007070cc495f64171711
Sergio Oller <sergioller@gmail.com>
2017-08-11 00:14:21 +0200

    vid_gb_ap voice plugin

--- src/vid_gb_ap.c
+++ src/vid_gb_ap.c
@@ -104,9 +104,8 @@ void voice_vid_gb_ap_plugin_exit()
 {
 }
 
-#ifdef MIMIC_ENABLE_PLUGINS
+#ifdef ENABLE_VID_GB_AP_BUILTIN
 #include "cst_plugins.h"
-
 mimic_plugin_t mimic_plugin = { "voice_vid_gb_ap", 0, &voice_vid_gb_ap_plugin_init, &voice_vid_gb_ap_plugin_exit};
 #endif
 
